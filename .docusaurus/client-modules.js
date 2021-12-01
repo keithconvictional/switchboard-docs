4fab49262088a5ed5a4a48092c912b43f5390fbc
keithconvictional <keith@convictional.com>
2021-12-01 12:33:04 -0500

    let there be light

---
+++ .docusaurus/client-modules.js
@@ -0,0 +1,6 @@
+export default [
+  require("/Users/keithweaver/Desktop/switchboard-docs/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/keithweaver/Desktop/switchboard-docs/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/keithweaver/Desktop/switchboard-docs/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
+  require("/Users/keithweaver/Desktop/switchboard-docs/src/css/custom.css"),
+];
